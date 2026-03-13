6ffeb437b148a0735db7ae22c3df0aa8481925cb
matthcol <matthias.colin@gmail.com>
2026-03-13 13:37:45 +0100

    filters

--- src/AppliWeb3/city.js
+++ src/AppliWeb3/city.js
@@ -6,12 +6,22 @@ const pageSize = 20
 const pageCount = Math.ceil(cityFiltered.length / pageSize)
 let cityPage = []
 
+const comparatorSortFr = new Intl.Collator('fr-FR').compare
+const comparatorSearchFr = new Intl.Collator(
+    'fr-FR', 
+    {
+        sensitivity: 'base',
+        usage: 'search'
+    }).compare
+
 // page prev/next controls :
 const btnPagePrev = document.getElementById("btn-page-prev")
 const btnPageNext = document.getElementById("btn-page-next")
+const btnSortName = document.getElementById("btn-sort-name")
 const btnSortDept = document.getElementById("btn-sort-dept")
 const btnSortPopDesc = document.getElementById("btn-sort-pop-desc")
 const btnSortPopAsc = document.getElementById("btn-sort-pop-asc")
+const inputSearch = document.getElementById('input-search')
 
 
 function loadPage(pageNum, pageSize){
@@ -20,6 +30,60 @@ function loadPage(pageNum, pageSize){
 
 function sortByPopulationDesc(){
     cityFiltered.sort((city1, city2) => city2.population - city1.population)
+}
+
+function sortByPopulationAsc(){
+    cityFiltered.sort((city1, city2) => city1.population - city2.population)
+}
+
+function sortByDept(){
+    cityFiltered.sort((city1, city2) => comparatorSortFr(city1.dep_code, city2.dep_code))
+}
+
+function sortByName(){
+    cityFiltered.sort((city1, city2) => comparatorSortFr(city1.nom, city2.nom))
+}
+
+function isNumber(text){
+    return !Number.isNaN(Number(text))
+}
+
+function isCodePostal(query){
+    return (query.length == 5) && isNumber(query)
+}
+
+function isCodeDept(query){
+    const codeU = query.toUpperCase()
+    return (codeU == '2A') 
+        || (codeU == '2B')
+        || (
+                [2,3].includes(codeU.length)
+                && isNumber(codeU)
+        )
+}
+
+function searchCityByCP(query){
+    cityFiltered = cities.filter(city => city.code_postal == query.toUpperCase())
+}
+
+function searchCityByDept(query){
+    cityFiltered = cities.filter(city => city.dep_code == query)
+}
+
+function searchCityByName(query){
+    // TODO: improve with partial content
+    cityFiltered = cities.filter(city => comparatorSearchFr(city.nom, query) == 0)
+}
+
+function searchCity(query){
+    const queryNum = parseInt(query)
+    if (isCodePostal(query)){
+        searchCityByCP(query)
+    } else if (isCodeDept(query)) {
+        searchCityByDept(query)
+    } else {
+        searchCityByName(query)
+    }
 }
 
 function cityCard(city) {
@@ -84,11 +148,55 @@ btnPageNext.addEventListener('click', () => {
 })
 
 btnSortPopDesc.addEventListener('click', () => {
+    console.log('sort by population desc')
     // sort
     sortByPopulationDesc()
     // back to page 0
     pageNum = 0
     // refresh
+    updatePage()
+})
+
+btnSortPopAsc.addEventListener('click', () => {
+    console.log('sort by population asc')
+    // sort
+    sortByPopulationAsc()
+    // back to page 0
+    pageNum = 0
+    // refresh
+    updatePage()
+})
+
+btnSortDept.addEventListener('click', () => {
+    console.log('sort by dept')
+    // sort
+    sortByDept()
+    // back to page 0
+    pageNum = 0
+    // refresh
+    updatePage()
+})
+
+btnSortName.addEventListener('click', () => {
+    console.log('sort by nom')
+    // sort
+    sortByName()
+    // back to page 0
+    pageNum = 0
+    // refresh
+    updatePage()
+})
+
+
+inputSearch.addEventListener('input', () => {
+    const query = inputSearch.value.trim()
+    console.log('Search city query:', query)
+    if (query.length > 0) {
+        searchCity(query)
+    } else {
+        cityFiltered = cities
+    }
+    pageNum = 0
     updatePage()
 })
 
